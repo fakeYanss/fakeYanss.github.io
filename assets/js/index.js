428796f6f548e848ffc601d92b75a5362dd7e9ec
fakeyanss <yanshisangc@gmail.com>
2020-08-09 06:56:47 +0800

    update style

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,5 +11,6 @@ $(document).ready(function(){
   setTimeout(printPhoto, 500);
 
   console.log('© zchen9 🙋 2015-' + (new Date()).getFullYear());
+  console.log('特别鸣谢zchen9小仙女的精彩主题，希望大家去逛逛她的github：https://github.com/zchen9');
   
 });
