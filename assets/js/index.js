41a9da3411e2d51f143dcfc722ff54e5f424b082
fakeYanss <yanshisangc@gmail.com>
2020-08-10 16:22:29 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
   var $shutter = $('#cameraShutter');
+  
+  var photoImg = document.getElementsByClassName("info-photo");
 
+  var timer = setTimeOut(function(){
+      photoImg.src = "./images/tequila-sunrise.jpg";
+    }, 2000);
+  
   function printPhoto() {
     $('.info-content').toggleClass('animate');
     $('.camera .glass').toggleClass('active');
-    
-    var photoImg = document.getElementsByClassName("info-photo");
-    oImg.onload = function () {
+
+    photoImg.onload = function () {
       clearTimeOut(timer);
       timer = null;
     }
   }
-  
-  var timer = setTimeOut(function(){
-      photoImg.src = "./images/tequila-sunrise.jpg";
-    }, 2000);
   
   $shutter.on('click', printPhoto);
 
